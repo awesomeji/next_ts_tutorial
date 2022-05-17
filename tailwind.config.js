102dbfaed9ba3af9dfbc19cb69143b8f34c1824b
GEOkettle <jsonmiou@gmail.com>
2022-05-17 16:45:26 +0900

    vercel

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    mode : 'jit',
+    // use tailwind on the files in below path
+  content: [ "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
